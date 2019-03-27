e8d5ecdb1800dbec43168161a6aa79338e60a4fc
hirarira <hirarira617@gmail.com>
2019-03-28 03:07:52 +0900

    promise-racceを追加

---
+++ promise/promise-race.js
@@ -0,0 +1,19 @@
+"use stuct";
+
+// promise
+let server = "http://localhost:5000";
+let request = require('request-promise');
+let pathes = ['/test1','/test2','/test3','/test4','/test5'];
+let promise_list = [];
+
+for(let i=0; i<pathes.length; i++){
+  promise_list.push(request(server+pathes[i]));
+}
+
+Promise.race(promise_list)
+.then((res)=>{
+  console.log(res);
+})
+.catch((e)=>{
+  console.log(e);
+});
