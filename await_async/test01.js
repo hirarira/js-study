1042cd12b49f986b2d81ef4e72c6c230ad153116
hirarira <hirarira617@gmail.com>
2019-10-14 17:43:17 +0900

    await/asyncの使い方を追加

--- await_async/test01.js
+++ await_async/test01.js
@@ -2,7 +2,7 @@
 
 let server = "http://localhost:5000";
 let request = require('request-promise');
-let path = ['/test1','/test2','/test3','/test4','/test5'];
+let path = ['/test1','/test2','/test3','/test4','/test5','/null'];
 
 // promiseを使う方法
 function test_promise(idx){
@@ -17,6 +17,17 @@ function test_promise(idx){
   });
 }
 
+// asyncを使う方法
+async function test_async(idx){
+  try{
+    let res = await request(server+path[idx]);
+    return res;
+  } catch(e) {
+    return e
+  }
+}
+
+
 // 単体で使う場合
 request(server+path[0])
 .then((res)=>{
@@ -25,6 +36,17 @@ request(server+path[0])
 })
 .then((res)=>{
   console.log(res);
+  return test_async(2);
+})
+.then((res)=>{
+  console.log(res);
+  return test_async(5);
+})
+.then((res)=>{
+  console.log(res.statusCode);
+})
+.catch((e)=>{
+  console.log(e);
 });
 
 
